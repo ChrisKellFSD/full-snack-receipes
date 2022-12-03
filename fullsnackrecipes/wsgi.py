2c149eb6ecfb70da1cfcac050792a9ab72d56653
ChrisKellFSD <chriskellcsn@gmail.com>
2022-12-03 16:08:20 +0000

    Installed the initial databases to set up project

---
+++ fullsnackrecipes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for fullsnackrecipes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fullsnackrecipes.settings')
+
+application = get_wsgi_application()
