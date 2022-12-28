28e822d4f6cf229420cefe958913a34defea97f5
ChrisKellFSD <chriskellcsn@gmail.com>
2022-12-28 14:43:54 +0000

    Run automated testing on the forms, models and views.py

--- recipes/test_models.py
+++ recipes/test_models.py
@@ -16,19 +16,7 @@ class TestModels(TestCase):
             method='This is an example method.'
             )
         self.assertEqual(str(recipe), 'Example Recipe')
-
-    # def test_get_absolute_url(self):
-    #     author = User.objects.create(username='example_username')
-    #     recipe = Recipe.objects.create(
-    #         title='Example Recipe',
-    #         slug='example-recipe',
-    #         author=author,
-    #         excerpt='This is an example recipe.',
-    #         ingredients='This is an example ingredient.',
-    #         method='This is an example method.'
-    #         )
-    
-    #     self.assertEqual(recipe.get_absolute_url(), '/recipe_detail/example-recipe')
+        
 
     def test_can_create_comment(self):
 
@@ -42,13 +30,11 @@ class TestModels(TestCase):
             method='This is an example method.'
             )
     
-    # Create a new Comment instance
         comment = Comment.objects.create(
             post=recipe,
             name='Example Commenter',
             email='example@example.com',
             body='This is an example comment.',
             )
-    
-        # Assert that the Comment instance was saved to the database
+
         self.assertIsNotNone(comment.id)
--- recipes/test_views.py
+++ recipes/test_views.py
@@ -20,34 +20,18 @@ class TestViews(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'my_recipes.html')
 
-    # def test_get_add_recipes_page(self):
-    #     self.client.login()
-    #     response = self.client.get('/add_recipe/')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'recipe_form.html')
-
-    # def test_get_recipe_detail_page(self):
-    #     recipe = Recipe.objects.create(title='Test Recipe', excerpt='test', ingredients='test', method='test')
-    #     response = self.client.get(f'/recipe_detail/{recipe.slug}')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'recipe_detail.html')
-
-    # def test_get_update_recipe_page(self):
-    #     recipe = Recipe.objects.create(title='Test Recipe', excerpt='test', ingredients='test', method='test')
-    #     response = self.client.get(f'/update_recipe/{self.recipe.slug}')
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'update_recipe.html')
 
     def test_get_searched_recipes_page(self):
         response = self.client.get('/search_recipes/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'search_recipes.html')
 
-    def test_get_your_recipes_page_if_user_logged_out(self):
-        self.client.logout()
+    def test_get_your_recipes_page_if_user_logged_in(self):
+        self.client.login()
         response = self.client.get('/my_recipes/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'my_recipes.html')
+
 
     def test_can_add_recipe(self):
         self.client.post('/add_recipe/', {
@@ -58,29 +42,6 @@ class TestViews(TestCase):
         })
         new_recipe = Recipe.objects.filter(title='Test Title')
         self.assertEqual(len(new_recipe), 0)
-
-    # def test_can_edit_recipe(self):
-    #     self.client.post(f'/update_recipe/{self.recipe.slug}', {
-    #         'title': 'Edited Title',
-    #         'excerpt': 'Test Excerpt',
-    #         'ingredients': 'Test Ingredients',
-    #         'method': 'Test Method'
-    #     })
-    #     edited_recipe = Recipe.objects.first().title
-    #     self.assertEqual(edited_recipe, "Edited Title")
-
-    # def test_can_delete_recipe(self):
-    #     author = User.objects.create(username='example_username')
-    #     recipe = Recipe.objects.create(
-    #         title='Example Recipe',
-    #         slug='example-recipe',
-    #         author=author,
-    #         excerpt='This is an example recipe.',
-    #         ingredients='This is an example ingredient.',
-    #         method='This is an example method.'
-    #         )
-    #     response = self.client.get(f'/delete_recipe/{recipe.slug}')
-    #     self.assertRedirects(response, '/my_recipes/')
 
     def test_number_of_likes(self):
 
@@ -94,15 +55,13 @@ class TestViews(TestCase):
             method='This is an example method.'
             )
     
-    # Create a few User instances to use as 'likes' for the Recipe
         user_1 = User.objects.create(username='user_1')
         user_2 = User.objects.create(username='user_2')
         user_3 = User.objects.create(username='user_3')
+        user_4 = User.objects.create(username='user_4')
+        user_5 = User.objects.create(username='user_5')
     
-    # Add the 'likes' to the Recipe instance
-        recipe.likes.add(user_1, user_2, user_3)
-    
-    # Assert that the number_of_likes() method returns the correct number of likes
-        self.assertEqual(recipe.number_of_likes(), 3)
+        recipe.likes.add(user_1, user_2, user_3, user_4, user_5)
+        self.assertEqual(recipe.number_of_likes(), 5)
 
 
